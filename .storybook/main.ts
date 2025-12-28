0da45c872028116b7c37ed42c427bf0a4ee9af18
badahertz52 <badahertz52@gmail.com>
2025-12-28 17:54:56 +0900

    ci: 스토리북 설정 업데이트 및 불필요한 패키지 제거
    - @storybook/addon-interactions 추가
    - @storybook/addon-essentials 외의 불필요한 패키지 제거
    - 스토리북 프레임워크 설정 방식 변경

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,14 @@
 import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-essentials"],
-  framework: "@storybook/nextjs-vite",
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs-vite",
+    options: {},
+  },
 };
 export default config;
