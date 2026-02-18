da11fe11fea0f3b3f429a0e1e238ae00a2e7cd8c
badahertz52 <badahertz52@gmail.com>
2026-02-18 22:03:06 +0900

    refactor: CustomIcon 컴포넌트와 하위 아이콘들 폴더 구조 변경

---
+++ src/components/Icon/CustomIcon/index.tsx
@@ -0,0 +1,41 @@
+import type React from "react";
+
+import type { CustomIconName } from "../types";
+import BadaIcon from "./icons/BadaIcon";
+import GitHubIcon from "./icons/GitHubIcon";
+import LinkedInIcon from "./icons/LinkedInIcon";
+
+type CustomIconComponent = React.FC<React.SVGProps<SVGSVGElement>>;
+
+const customIconMap: Record<
+  Exclude<CustomIconName, "bada">,
+  CustomIconComponent
+> = {
+  github: GitHubIcon,
+  linkedin: LinkedInIcon,
+};
+
+interface CustomIconProps extends React.SVGProps<SVGSVGElement> {
+  name: CustomIconName;
+}
+
+export default function CustomIcon({
+  name,
+  width = 24,
+  height = 24,
+  ...rest
+}: CustomIconProps) {
+  // bada는 이미지 기반이므로 별도 처리
+  if (name === "bada") {
+    return (
+      <BadaIcon
+        width={Number(width)}
+        height={Number(height)}
+        className={rest.className}
+      />
+    );
+  }
+
+  const IconComponent = customIconMap[name];
+  return <IconComponent width={width} height={height} {...rest} />;
+}
