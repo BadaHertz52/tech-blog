79b4a2a1c30c088b3196230baf9a474d1db4fcca
badahertz52 <badahertz52@gmail.com>
2026-02-18 21:12:39 +0900

    style: 프리텐다드 폰트 설정

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+
+import "./globals.css";
+
+const pretendard = localFont({
+  src: "../assets/fonts/pretendard/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
+export const metadata: Metadata = {
+  title: "BADA - Tech Blog",
+  description: "개발자 기술 블로그",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko" className={pretendard.variable}>
+      <body className={pretendard.className}>{children}</body>
+      <footer>테스트</footer>
+    </html>
+  );
+}
