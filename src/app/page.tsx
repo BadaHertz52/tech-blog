4d1fa2c352a4badacf554d809c68df00c740aa4e
badahertz52 <badahertz52@gmail.com>
2026-02-19 03:25:38 +0900

    feat: 홈 페이지 구현 및 룰렛 관련 coming soon 말풍선 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 import Image from "next/image";
 
 import heroImage from "@/assets/images/hero.webp";
+import SpeechBubble from "@/components/SpeechBubble";
 
 export default function Home() {
   return (
-    <div className="relative w-[87vw]">
+    <div className="relative file:w-full">
       <Image
         src={heroImage}
         alt="데스트 셋업"
         priority
         placeholder="blur"
-        sizes="87vw"
+        sizes="100%"
         className="h-auto w-full rounded-md"
       />
+      <button
+        type="button"
+        disabled
+        className="xs:top-[36%] absolute left-[20%] top-[34%] translate-x-[-50%] translate-y-[-50%] text-[12px] leading-[1.4] md:top-[36%] md:text-sm [@media(max-width:320px)]:top-[32%]"
+      >
+        <SpeechBubble tailPosition="bottom">Coming Soon</SpeechBubble>
+      </button>
     </div>
   );
 }
