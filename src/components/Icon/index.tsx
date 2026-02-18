97e5307e8c0802088a3f579e9a7b281d881ba394
badahertz52 <badahertz52@gmail.com>
2026-02-18 22:16:09 +0900

    style: 아이콘 기본 색상으로 차콜 적용

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -10,11 +10,12 @@ export default function Icon({
   name,
   width = 24,
   height = 24,
+  color = "var(--color-gray-charcoal)",
   ...rest
 }: IconProps) {
   if (isLucideIconName(name)) {
-    return <LucideIcon name={name} width={width} height={height} {...rest} />;
+    return <LucideIcon name={name} width={width} height={height} color={color} {...rest} />;
   }
 
-  return <CustomIcon name={name} width={width} height={height} {...rest} />;
+  return <CustomIcon name={name} width={width} height={height} color={color} {...rest} />;
 }
