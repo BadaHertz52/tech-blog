a623ff639a40aa0ee9abf6ae88a6cf4e7208b801
badahertz52 <badahertz52@gmail.com>
2026-03-02 15:26:36 +0900

    feat: 전체 페이지 404 UI 추가 (app/not-found.tsx)
    EmptyState 컴포넌트 재사용, 홈으로 이동 버튼 제공

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import ButtonLink from "@/components/ButtonLink";
+import EmptyState from "@/components/EmptyState";
+import { ROUTES } from "@/constants/paths";
+
+export default function NotFound() {
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <EmptyState>
+        <EmptyState.Icon />
+        <EmptyState.Content>
+          <p>페이지를 찾을 수 없어요.</p>
+        </EmptyState.Content>
+        <EmptyState.Actions>
+          <ButtonLink href={ROUTES.home} variant="primary">
+            홈으로 이동
+          </ButtonLink>
+        </EmptyState.Actions>
+      </EmptyState>
+    </div>
+  );
+}
