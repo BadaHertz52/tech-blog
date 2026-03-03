2b636c7e956a9ab77d16bc19ff3f5dd111910934
badahertz52 <badahertz52@gmail.com>
2026-03-03 22:41:32 +0900

    fix: Provider 컴포넌트 RCC로 변경 및 컴포넌트 선언 컨벤션 적용안된 것 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import type React from "react";
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import { Providers } from "./providers";
+import Providers from "./providers";
 import "./globals.css";
 
 const pretendard = localFont({
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import { OverlayProvider } from "overlay-kit";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return <OverlayProvider>{children}</OverlayProvider>;
 }
