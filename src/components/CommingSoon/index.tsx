e04e936673ed98b4a71602ca88a89fddd0b43dec
badahertz52 <badahertz52@gmail.com>
2026-03-28 18:03:13 +0900

    feat: about-me 페이지 추가 (CommingSoon 컴포넌트 적용)

--- src/components/CommingSoon/index.tsx
+++ src/components/CommingSoon/index.tsx
@@ -6,12 +6,16 @@ import { ROUTES } from "@/constants/paths";
 
 export default function CommingSoon() {
   return (
-    <div className="flex h-auto w-[320px] flex-col items-center justify-center gap-[22px]">
+    <div className="flex h-full w-full flex-col items-center justify-center gap-[22px] rounded-card bg-white px-[40px] pt-[40px]">
       <div className="flex flex-col items-center gap-6">
         <p className="text-center text-[24px] font-bold leading-[1.4] tracking-[-0.5px] text-[#7e7e7e]">
           현재 준비 중입니다. 곧 만나요!
         </p>
-        <ButtonLink href={ROUTES.home} variant="secondary">
+        <ButtonLink
+          href={ROUTES.home}
+          variant="secondary"
+          className="text-[#7e7e7e]"
+        >
           홈으로 이동하기
         </ButtonLink>
       </div>
