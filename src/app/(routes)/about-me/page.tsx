e9e177ffafd644774d5e7d867648534a3f5998e1
badahertz52 <badahertz52@gmail.com>
2026-05-15 18:24:52 +0900

    refactor: main 레이아웃을 flex-col로 변경하여 홈·about-me 페이지 중앙 정렬 개선

--- src/app/(routes)/about-me/page.tsx
+++ src/app/(routes)/about-me/page.tsx
@@ -2,7 +2,7 @@ import ComingSoon from "@/components/ComingSoon";
 
 export default function AboutMePage() {
   return (
-    <div className="absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 p-xl">
+    <div className="flex flex-1 items-center justify-center">
       <ComingSoon />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomeDeskScene from "@/app/_components/HomeDeskScene";
 
 export default function Home() {
   return (
-    <div className="absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 px-lg md:px-xl">
+    <div className="flex flex-1 items-center justify-center">
       <HomeDeskScene />
     </div>
   );
